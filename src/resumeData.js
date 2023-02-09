5b2ce8a78f4e758d9d73e3da72328714a905e88d
Ryan <ryanlu294@gmail.com>
2023-02-09 10:09:02 -0800

    updates my resume

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,106 +1,114 @@
 let resumeData = {
-    "imagebaseurl":"https://rbhatia46.github.io/",
-    "name": "Rahul Bhatia",
-    "role": "Frontend Developer and Data Scientist",
-    "linkedinId":"Your LinkedIn Id",
-    "skypeid": "Your skypeid",
-    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-    "socialLinks":[
-        {
-          "name":"linkedin",
-          "url":"https://www.linkedin.com/in/rahul-bhatia-67ba08121/",
-          "className":"fa fa-linkedin"
-        },
-        {
-          "name":"github",
-          "url":"http://github.com/rbhatia46",
-          "className":"fa fa-github"
-        },
-        {
-          "name":"skype",
-          "url":"http://twitter.com/rbhatia46",
-          "className":"fa fa-twitter"
-        }
-      ],
-    "aboutme":"I am currently a pre-final year student at The LNM Institute of Information Technology and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving deeper into Machine Learning. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
-    "address":"India",
-    "website":"https://rbhatia46.github.io",
-    "education":[
-      {
-        "UniversityName":"The LNM Insitute of Information Technology",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Aug",
-        "YearOfPassing":"2020",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "UniversityName":"Some University",
-        "specialization":"Some specialization",
-        "MonthOfPassing":"Jan",
-        "YearOfPassing":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "work":[
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      },
-      {
-        "CompanyName":"Some Company",
-        "specialization":"Some specialization",
-        "MonthOfLeaving":"Jan",
-        "YearOfLeaving":"2018",
-        "Achievements":"Some Achievements"
-      }
-    ],
-    "skillsDescription":"Your skills here",
-    "skills":[
-      {
-        "skillname":"HTML5"
-      },
-      {
-        "skillname":"CSS"
-      },
-      {
-        "skillname":"Reactjs"
-      }
-    ],
-    "portfolio":[
-      {
-        "name":"project1",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      },
-      {
-        "name":"project2",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/project.jpg"
-      },
-      {
-        "name":"project3",
-        "description":"mobileapp",  
-        "imgurl":"images/portfolio/project2.png"
-      },
-      {
-        "name":"project4",
-        "description":"mobileapp",
-        "imgurl":"images/portfolio/phone.jpg"
-      }
-    ],
-    "testimonials":[
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      },
-      {
-        "description":"This is a sample testimonial",
-        "name":"Some technical guy"
-      }
-    ]
-  }
-  
-  export default resumeData
+  imagebaseurl: "https://ryanlu294.github.io/",
+  name: "Ryan Lu",
+  role: "full stack developer",
+  linkedinId: "ryanlu294",
+  skypeid: "Your skypeid",
+  roleDescription:
+    "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/ryanlu294/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "http://github.com/ryanlu294",
+      className: "fa fa-github",
+    },
+    // {
+    //   "name":"skype",
+    //   "url":"http://twitter.com/rbhatia46",
+    //   "className":"fa fa-twitter"
+    // }
+  ],
+  aboutme:
+    "I'm Ryan, a full stack software engineer that loves to spend time debugging code and build software with users in mind. I believe the key to an amazing application is taking action to solve problems autonomously and active communication between team members. I strive to inspire those around me and provide unique insights to difficult problems. In my spare time, I love playing video games and building mechanical keyboards!",
+  address: "Riverside, CA",
+  website: "https://ryanlu294.github.io",
+  education: [
+    {
+      UniversityName: "Hack Reactor",
+      specialization: "Software Engineering Certificate",
+      MonthOfPassing: "Feb",
+      YearOfPassing: "2023",
+      // Achievements: "Some Achievements",
+    },
+    {
+      UniversityName: "University of California, Los Angeles",
+      specialization: "Geography, B.S.",
+      MonthOfPassing: "Dec",
+      YearOfPassing: "2020",
+      // Achievements: "Some Achievements",
+    },
+  ],
+  // work: [
+  //   {
+  //     CompanyName: "Some Company",
+  //     specialization: "Some specialization",
+  //     MonthOfLeaving: "Jan",
+  //     YearOfLeaving: "2018",
+  //     Achievements: "Some Achievements",
+  //   },
+  //   {
+  //     CompanyName: "Some Company",
+  //     specialization: "Some specialization",
+  //     MonthOfLeaving: "Jan",
+  //     YearOfLeaving: "2018",
+  //     Achievements: "Some Achievements",
+  //   },
+  // ],
+  // skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "Reactjs",
+    },
+    {
+      skillname: "Javascript",
+    },
+    {
+      skillname: "Python",
+    },
+    {
+      skillname: "HTML5",
+    },
+    {
+      skillname: "CSS",
+    },
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+  ],
+  // testimonials: [
+  //   {
+  //     description: "This is a sample testimonial",
+  //     name: "Some technical guy",
+  //   },
+  //   {
+  //     description: "This is a sample testimonial",
+  //     name: "Some technical guy",
+  //   },
+  // ],
+};
+
+export default resumeData;
